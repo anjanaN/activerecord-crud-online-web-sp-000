8b2065bb189ba78822d7139dc068f734d26247a6
Anjana Nair <anju740@gmail.com>
2020-04-07 02:03:00 +0000

    Automatically backed up by Learn

--- db/migrate/20200407020146_create_movies.rb
+++ db/migrate/20200407020146_create_movies.rb
@@ -3,8 +3,8 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |t|
       t.string :title
       t.integer :release_date
-      t.string :image_url
-      t.string :size
+      t.string :director
+      t.string :lead
       t.timestamps
     end
   end
